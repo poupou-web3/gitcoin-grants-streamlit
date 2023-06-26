0534326784cdf84f9fc2492777f08fde4bf4d81f
Poupou <108153089+poupou-web3@users.noreply.github.com>
2023-06-26 23:07:03 +0200

    Start refactoring

---
+++ functions.py
@@ -0,0 +1,161 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+import datetime
+import plotly.graph_objs as go
+import plotly.express as px
+import locale
+
+@st.cache_data(ttl=3000)
+def load_chain_data(chain_id):
+    chain_url = 'https://indexer-grants-stack.gitcoin.co/data/' + chain_id + '/rounds.json'
+    try:
+        response = requests.get(chain_url)
+        if response.status_code == 200:
+            chain_data = response.json()
+            st.text(chain_data)
+            rounds = []
+            for round in chain_data:
+                if round['metadata'] is not None:
+                    round_data = {
+                        'round_id': round['id'],
+                        'name': round['metadata']['name'],
+                        'amountUSD': round['amountUSD'],
+                        'votes': round['votes'],
+                        'description': round['metadata']['description'] if 'description' in round['metadata'] else '',
+                        'matchingFundsAvailable': round['metadata']['matchingFunds']['matchingFundsAvailable'] if 'matchingFunds' in round['metadata'] else '',
+                        'matchingCap': round['metadata']['matchingFunds']['matchingCap'] if 'matchingFunds' in round['metadata'] else '',
+                        'roundStartTime': datetime.datetime.utcfromtimestamp(int(round['roundStartTime'])), # create a datetime object from the timestamp in UTC time
+                        'roundEndTime': datetime.datetime.utcfromtimestamp(int(round['roundEndTime']))
+                    }
+                    rounds.append(round_data)
+            df = pd.DataFrame(rounds)
+            st.table(df)
+            # Filter to beta rounds
+            start_time = datetime.datetime(2023, 4, 26, 15, 0, 0)
+            end_time = datetime.datetime(2023, 5, 9, 23, 59, 0)
+            # filter to only include rounds with votes > 0 and roundStartTime <= start_time and roundEndTime == end_time
+            df = df[(df['votes'] > 0) & (df['roundStartTime'] <= start_time) & (df['roundEndTime'] == end_time)]
+            return df 
+    except: 
+        return pd.DataFrame()
+
+@st.cache_data(ttl=3000)
+def load_round_projects_data(round_id):
+    # prepare the URLs
+    projects_url = 'https://indexer-grants-stack.gitcoin.co/data/1/rounds/' + round_id + '/projects.json'
+    
+    try:
+        # download the Projects JSON data from the URL
+        response = requests.get(projects_url)
+        if response.status_code == 200:
+            projects_data = response.json()
+
+        # Extract the relevant data from each project
+        projects = []
+        for project in projects_data:
+            project_data = {
+                'id': project['id'],
+                'title': project['metadata']['application']['project']['title'],
+                'description': project['metadata']['application']['project']['description'],
+                'status': project['status'],
+                'amountUSD': project['amountUSD'],
+                'votes': project['votes'],
+                'uniqueContributors': project['uniqueContributors']
+            }
+            projects.append(project_data)
+        # Create a DataFrame from the extracted data
+        dfp = pd.DataFrame(projects)
+        # Reorder the columns to match the desired order and rename column id to project_id
+        dfp = dfp[['id', 'title', 'description', 'status', 'amountUSD', 'votes', 'uniqueContributors']]
+        dfp = dfp.rename(columns={'id': 'project_id'})
+        # Filter to only approved projects
+        dfp = dfp[dfp['status'] == 'APPROVED']
+        return dfp
+    except:
+        return pd.DataFrame()
+    
+@st.cache_data(ttl=3000)
+def load_round_votes_data(round_id):
+    votes_url = 'https://indexer-grants-stack.gitcoin.co/data/1/rounds/' + round_id + '/votes.json'
+    try:
+        # download the Votes JSON data from the URL
+        response = requests.get(votes_url)
+        if response.status_code == 200:
+            votes_data = response.json()
+        df = pd.DataFrame(votes_data)
+        return df
+    except:
+        return pd.DataFrame()
+
+
+def get_USD_by_round_chart(chain_data, color_map):
+    grouped_data = chain_data.groupby('name')['amountUSD'].sum().reset_index().sort_values('amountUSD', ascending=True)
+    data = [go.Bar(
+        x=grouped_data['amountUSD'], 
+        y=grouped_data['name'],
+        marker_color=grouped_data['name'].map(color_map), # map each round to a specific color
+        orientation='h'
+    )]
+    layout = go.Layout(
+        title='Crowdfunded (in $) by Round',
+        xaxis=dict(title='Dollars'),
+        yaxis=dict(title='Round')
+    )
+    fig = go.Figure(data=data, layout=layout)
+    return fig
+
+def get_contributions_by_round_bar_chart(chain_data, color_map):
+    grouped_data = chain_data.groupby('name')['votes'].sum().reset_index().sort_values('votes', ascending=True)
+    data = [go.Bar(
+        x=grouped_data['votes'], 
+        y=grouped_data['name'],
+        marker_color=grouped_data['name'].map(color_map), # map each round to a specific color
+        orientation='h'
+    )]
+    layout = go.Layout(
+        title='Total Contributions (#) by Round',
+        xaxis=dict(title='Number'),
+        yaxis=dict(title='Round')
+    )
+    fig = go.Figure(data=data, layout=layout)
+    return fig
+
+def create_color_map(chain_data):
+    unique_rounds = chain_data['name'].unique()
+    color_list = ['#4C72B0', '#DD8452', '#55A868', '#C44E52', '#8172B2', '#937860', '#DA8BC3', '#8C8C8C', '#CCB974', '#64B5CD', '#4E3D3D', '#AEBD38', '#AD6B5E', '#1F78B4', '#B2DF8A'] # manually specified list of colors
+    color_map = dict(zip(unique_rounds, color_list[:len(unique_rounds)])) # map each round to a specific color
+    return color_map
+
+
+def get_grants_bar_chart(votes_data):
+    grouped_data = votes_data.groupby('title')['amountUSD'].sum().reset_index().sort_values('amountUSD', ascending=True)
+
+    data = [go.Bar(
+        x=grouped_data['amountUSD'], 
+        y=grouped_data['title'],
+        marker_color='blue',
+        orientation='h'
+    )]
+    layout = go.Layout(
+        title='Total Contributions (in $) by Grant',
+        xaxis= { 'title':'Total Contributions ($)'},
+        yaxis={'title':'Grant'},
+        height=800
+
+    )
+    fig = go.Figure(data=data, layout=layout)
+    return fig
+
+def create_block_times(starting_blockNumber, ending_blockNumber, starting_blockTime):
+    # Create an array of 107,000 blockNumbers starting from the starting_blockNumber and incrementing by 1 each time
+    blocks = np.arange(starting_blockNumber, ending_blockNumber, 1)
+    # create a new dataframe with the blocks array
+    df = pd.DataFrame(blocks)
+    df.columns = ['blockNumber']
+    # create a new column called utc_time and use the starting_blockTime as the value for the first starting_blockNumber
+    df['utc_time'] = starting_blockTime
+    # as the blockNumber increases by 1, add 12.133 seconds to the utc_time
+    df['utc_time'] = pd.to_datetime(df['utc_time']) + pd.to_timedelta(12.13*(df['blockNumber'] - starting_blockNumber), unit='s')
+    return df
--- gitcoin-grants.py
+++ gitcoin-grants.py
@@ -1,112 +1,36 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import requests
 import datetime
-import plotly.graph_objs as go
 import plotly.express as px
 import locale
+from functions import *
 
 st.set_page_config(
-    page_title="Gitcoin Beta Rounds",
+    page_title="Gitcoin Rounds",
     page_icon="📊",
     layout="wide",
-
 )
 
-st.title('Gitcoin Beta Rounds')
-st.write('The Gitcoin Grants Program is a quarterly initiative that empowers everyday believers to drive funding toward what they believe matters, with the impact of individual donations being magnified by the use of the [Quadratic Funding (QF)](https://wtfisqf.com) distribution mechanism.')
-st.write('You can donate to projects in the Beta Round from April 25th 2023 12:00 UTC to May 9th 2023 23:59 UTC.')
-st.write('👉 Visit [grants.gitcoin.co](https://grants.gitcoin.co) to donate.')
+round = st.radio("Select Round ", ('Beta', 'Community'))
 
+if round == 'Beta':
+    chain_id = '1'
 
-chain_id = '1'
+    st.title('Gitcoin Beta Rounds')
+    st.write('The Gitcoin Grants Program is a quarterly initiative that empowers everyday believers to drive funding toward what they believe matters, with the impact of individual donations being magnified by the use of the [Quadratic Funding (QF)](https://wtfisqf.com) distribution mechanism.')
+    st.write('You can donate to projects in the Beta Round from April 25th 2023 12:00 UTC to May 9th 2023 23:59 UTC.')
+    st.write('👉 Visit [grants.gitcoin.co](https://grants.gitcoin.co) to donate.')
 
+elif round == 'Community':
+    chain_id = '10'
 
-@st.cache_data(ttl=3000)
-def load_chain_data(chain_id):
-    chain_url = 'https://indexer-grants-stack.gitcoin.co/data/' + chain_id + '/rounds.json'
-    try:
-        response = requests.get(chain_url)
-        if response.status_code == 200:
-            chain_data = response.json()
-            rounds = []
-            for round in chain_data:
-                if round['metadata'] is not None:
-                    round_data = {
-                        'round_id': round['id'],
-                        'name': round['metadata']['name'],
-                        'amountUSD': round['amountUSD'],
-                        'votes': round['votes'],
-                        'description': round['metadata']['description'] if 'description' in round['metadata'] else '',
-                        'matchingFundsAvailable': round['metadata']['matchingFunds']['matchingFundsAvailable'] if 'matchingFunds' in round['metadata'] else '',
-                        'matchingCap': round['metadata']['matchingFunds']['matchingCap'] if 'matchingFunds' in round['metadata'] else '',
-                        'roundStartTime': datetime.datetime.utcfromtimestamp(int(round['roundStartTime'])), # create a datetime object from the timestamp in UTC time
-                        'roundEndTime': datetime.datetime.utcfromtimestamp(int(round['roundEndTime']))
-                    }
-                    rounds.append(round_data)
-            df = pd.DataFrame(rounds)
-            # Filter to beta rounds
-            start_time = datetime.datetime(2023, 4, 26, 15, 0, 0)
-            end_time = datetime.datetime(2023, 5, 9, 23, 59, 0)
-            # filter to only include rounds with votes > 0 and roundStartTime <= start_time and roundEndTime == end_time
-            df = df[(df['votes'] > 0) & (df['roundStartTime'] <= start_time) & (df['roundEndTime'] == end_time)]
-            return df 
-    except: 
-        return pd.DataFrame()
-
-@st.cache_data(ttl=3000)
-def load_round_projects_data(round_id):
-    # prepare the URLs
-    projects_url = 'https://indexer-grants-stack.gitcoin.co/data/1/rounds/' + round_id + '/projects.json'
-    
-    try:
-        # download the Projects JSON data from the URL
-        response = requests.get(projects_url)
-        if response.status_code == 200:
-            projects_data = response.json()
-
-        # Extract the relevant data from each project
-        projects = []
-        for project in projects_data:
-            project_data = {
-                'id': project['id'],
-                'title': project['metadata']['application']['project']['title'],
-                'description': project['metadata']['application']['project']['description'],
-                'status': project['status'],
-                'amountUSD': project['amountUSD'],
-                'votes': project['votes'],
-                'uniqueContributors': project['uniqueContributors']
-            }
-            projects.append(project_data)
-        # Create a DataFrame from the extracted data
-        dfp = pd.DataFrame(projects)
-        # Reorder the columns to match the desired order and rename column id to project_id
-        dfp = dfp[['id', 'title', 'description', 'status', 'amountUSD', 'votes', 'uniqueContributors']]
-        dfp = dfp.rename(columns={'id': 'project_id'})
-        # Filter to only approved projects
-        dfp = dfp[dfp['status'] == 'APPROVED']
-        return dfp
-    except:
-        return pd.DataFrame()
-    
-@st.cache_data(ttl=3000)
-def load_round_votes_data(round_id):
-    votes_url = 'https://indexer-grants-stack.gitcoin.co/data/1/rounds/' + round_id + '/votes.json'
-    try:
-        # download the Votes JSON data from the URL
-        response = requests.get(votes_url)
-        if response.status_code == 200:
-            votes_data = response.json()
-        df = pd.DataFrame(votes_data)
-        return df
-    except:
-        return pd.DataFrame()
+    st.title('Gitcoin Community Rounds')
 
 
 data_load_state = st.text('Loading data...')
 chain_data = load_chain_data(chain_id)
 data_load_state.text("")
+st.table(chain_data)
 
 st.subheader('Beta Rounds Summary')
 # create two-column metrics. One with the sum of votes and the other with the amountUSD
@@ -119,43 +43,6 @@ col3.metric('Total Rounds', '{:,.0f}'.format(chain_data['round_id'].count()))
 # filter chain_data to name, votes, amountUSD
 
 
-def get_USD_by_round_chart(chain_data, color_map):
-    grouped_data = chain_data.groupby('name')['amountUSD'].sum().reset_index().sort_values('amountUSD', ascending=True)
-    data = [go.Bar(
-        x=grouped_data['amountUSD'], 
-        y=grouped_data['name'],
-        marker_color=grouped_data['name'].map(color_map), # map each round to a specific color
-        orientation='h'
-    )]
-    layout = go.Layout(
-        title='Crowdfunded (in $) by Round',
-        xaxis=dict(title='Dollars'),
-        yaxis=dict(title='Round')
-    )
-    fig = go.Figure(data=data, layout=layout)
-    return fig
-
-def get_contributions_by_round_bar_chart(chain_data, color_map):
-    grouped_data = chain_data.groupby('name')['votes'].sum().reset_index().sort_values('votes', ascending=True)
-    data = [go.Bar(
-        x=grouped_data['votes'], 
-        y=grouped_data['name'],
-        marker_color=grouped_data['name'].map(color_map), # map each round to a specific color
-        orientation='h'
-    )]
-    layout = go.Layout(
-        title='Total Contributions (#) by Round',
-        xaxis=dict(title='Number'),
-        yaxis=dict(title='Round')
-    )
-    fig = go.Figure(data=data, layout=layout)
-    return fig
-
-def create_color_map(chain_data):
-    unique_rounds = chain_data['name'].unique()
-    color_list = ['#4C72B0', '#DD8452', '#55A868', '#C44E52', '#8172B2', '#937860', '#DA8BC3', '#8C8C8C', '#CCB974', '#64B5CD', '#4E3D3D', '#AEBD38', '#AD6B5E', '#1F78B4', '#B2DF8A'] # manually specified list of colors
-    color_map = dict(zip(unique_rounds, color_list[:len(unique_rounds)])) # map each round to a specific color
-    return color_map
 
 # create color map
 color_map = create_color_map(chain_data)
@@ -199,26 +86,6 @@ col2.write('## Projects')
 col2.write(dfp[['title', 'votes', 'amountUSD', 'uniqueContributors']])
 
 
-def get_grants_bar_chart(votes_data):
-    grouped_data = votes_data.groupby('title')['amountUSD'].sum().reset_index().sort_values('amountUSD', ascending=True)
-
-    data = [go.Bar(
-        x=grouped_data['amountUSD'], 
-        y=grouped_data['title'],
-        marker_color='blue',
-        orientation='h'
-    )]
-    layout = go.Layout(
-        title='Total Contributions (in $) by Grant',
-        xaxis= { 'title':'Total Contributions ($)'},
-        yaxis={'title':'Grant'},
-        height=800
-
-    )
-    fig = go.Figure(data=data, layout=layout)
-    return fig
-
-
 # display the chart
 # fig_grants = get_grants_bar_chart(dfv)
 # st.plotly_chart(fig_grants, use_container_width=False)
@@ -227,18 +94,6 @@ def get_grants_bar_chart(votes_data):
 starting_blockNumber = 17123133
 ending_blockNumber = dfv['blockNumber'].max()
 starting_blockTime = datetime.datetime(2023, 4, 25, 12, 13, 35)
-
-def create_block_times(starting_blockNumber, ending_blockNumber, starting_blockTime):
-    # Create an array of 107,000 blockNumbers starting from the starting_blockNumber and incrementing by 1 each time
-    blocks = np.arange(starting_blockNumber, ending_blockNumber, 1)
-    # create a new dataframe with the blocks array
-    df = pd.DataFrame(blocks)
-    df.columns = ['blockNumber']
-    # create a new column called utc_time and use the starting_blockTime as the value for the first starting_blockNumber
-    df['utc_time'] = starting_blockTime
-    # as the blockNumber increases by 1, add 12.133 seconds to the utc_time
-    df['utc_time'] = pd.to_datetime(df['utc_time']) + pd.to_timedelta(12.13*(df['blockNumber'] - starting_blockNumber), unit='s')
-    return df
 
 dfb = create_block_times(starting_blockNumber, ending_blockNumber, starting_blockTime)
 # merge the block times with the votes data
