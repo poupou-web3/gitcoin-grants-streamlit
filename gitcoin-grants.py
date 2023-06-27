260bc45f8722e1a39dbc01ec9a6ce074a2bd3d69
Poupou <108153089+poupou-web3@users.noreply.github.com>
2023-06-27 10:22:43 +0200

    Working community round

--- gitcoin-grants.py
+++ gitcoin-grants.py
@@ -11,17 +11,19 @@ st.set_page_config(
     layout="wide",
 )
 
-round = st.radio("Select Round ", ('Beta', 'Community'))
+round_name = st.radio("Select Round ", ('Beta', 'Community'))
 
-if round == 'Beta':
+if round_name == 'Beta':
     chain_id = '1'
 
     st.title('Gitcoin Beta Rounds')
-    st.write('The Gitcoin Grants Program is a quarterly initiative that empowers everyday believers to drive funding toward what they believe matters, with the impact of individual donations being magnified by the use of the [Quadratic Funding (QF)](https://wtfisqf.com) distribution mechanism.')
+    st.write('The Gitcoin Grants Program is a quarterly initiative that empowers everyday believers to drive funding '
+             'toward what they believe matters, with the impact of individual donations being magnified by the use of '
+             'the [Quadratic Funding (QF)](https://wtfisqf.com) distribution mechanism.')
     st.write('You can donate to projects in the Beta Round from April 25th 2023 12:00 UTC to May 9th 2023 23:59 UTC.')
     st.write('👉 Visit [grants.gitcoin.co](https://grants.gitcoin.co) to donate.')
 
-elif round == 'Community':
+elif round_name == 'Community':
     chain_id = '10'
 
     st.title('Gitcoin Community Rounds')
@@ -30,42 +32,40 @@ elif round == 'Community':
 data_load_state = st.text('Loading data...')
 chain_data = load_chain_data(chain_id)
 data_load_state.text("")
-st.table(chain_data)
 
-st.subheader('Beta Rounds Summary')
+st.subheader(round_name + ' Rounds Summary')
 # create two-column metrics. One with the sum of votes and the other with the amountUSD
 col1, col2, col3 = st.columns(3)
 col1.metric("Total Votes", '{:,.0f}'.format(chain_data['votes'].sum()))
 col2.metric('Total Contributed', '${:,.2f}'.format(chain_data['amountUSD'].sum()))
 col3.metric('Total Rounds', '{:,.0f}'.format(chain_data['round_id'].count()))
 
+if chain_data['round_id'].count() > 1:
+    # create color map
+    color_map = create_color_map(chain_data)
+    # create two-column charts. One with the sum of votes and the other with the amountUSD
+    col1, col2 = st.columns(2)
+    fig = get_USD_by_round_chart(chain_data, color_map)
+    col1.plotly_chart(fig, use_container_width=True)
+    fig = get_contributions_by_round_bar_chart(chain_data, color_map)
+    col2.plotly_chart(fig, use_container_width=True)
+    chain_data_display = chain_data[['name', 'votes', 'amountUSD']]
 
-# filter chain_data to name, votes, amountUSD
-
-
-
-# create color map
-color_map = create_color_map(chain_data)
-
-# create two-column charts. One with the sum of votes and the other with the amountUSD
-col1, col2 = st.columns(2)
-fig = get_USD_by_round_chart(chain_data, color_map)
-col1.plotly_chart(fig, use_container_width=True)
-fig = get_contributions_by_round_bar_chart(chain_data, color_map)
-col2.plotly_chart(fig, use_container_width=True)
-chain_data_display = chain_data[['name', 'votes', 'amountUSD']]
-
-st.subheader("Round Details")
-# selectbox to select the round
-option = st.selectbox(
-    'Select Round',
-    chain_data['name'])
+    st.subheader("Round Details")
+    # selectbox to select the round
+    option = st.selectbox(
+        'Select Round',
+        chain_data['name'])
+else:
+    option = chain_data['name'].values[0]
+    st.subheader("Round Details:")
+    st.subheader(option)
 
 data_load_state = st.text('Loading data...')
 # load round data for the option selected by looking up the round id with that name in the chain_data df
 round_id = chain_data[chain_data['name'] == option]['round_id'].values[0]
-dfp = load_round_projects_data('1', round_id)
-dfv = load_round_votes_data('1', round_id)
+dfp = load_round_projects_data(chain_id, round_id)
+dfv = load_round_votes_data(chain_id, round_id)
 data_load_state.text("")
 
 dfv = pd.merge(dfv, dfp[['project_id', 'title', 'status']], how='left', left_on='projectId', right_on='project_id')
